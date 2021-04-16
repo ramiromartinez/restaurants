0106914da91248be6333be2aa43ffe3fa54dc8af
Ramiro Martinez IQUS <ing.ramiromartinezm@gmail.com>
2021-04-16 14:37:42 -0400

    add navigation

--- App.js
+++ App.js
@@ -2,13 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+
+import Navigation from './navigations/Navigation';
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <Navigation/>
+  )
 }
 
 const styles = StyleSheet.create({
@@ -18,4 +18,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
