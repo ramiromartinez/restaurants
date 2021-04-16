54569a01ae9ce0000c0b09f72cd8b7fda406b7ee
Ramiro Martinez IQUS <ing.ramiromartinezm@gmail.com>
2021-04-16 16:57:19 -0400

    trabajo 1

---
+++ utils/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDmzkSQFgd_KGjqwmRqDE2BiAHRlQsOR1U",
+    authDomain: "restaurants-435f3.firebaseapp.com",
+    projectId: "restaurants-435f3",
+    storageBucket: "restaurants-435f3.appspot.com",
+    messagingSenderId: "499494897063",
+    appId: "1:499494897063:web:dc0fbfec518f10a7eba282"
+  }
+  // Initialize Firebase
+  export const firebaseApp =  firebase.initializeApp(firebaseConfig);
